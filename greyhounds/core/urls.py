d3c102e2c1191476163d89ac0e58a953d809e974
Matt <matt.leach25@gmail.com>
2014-12-04 22:47:31 +0000

    setting out a very basic website structure

--- greyhounds/core/urls.py
+++ greyhounds/core/urls.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
+    url(r'^history/', 'core.views.just_render', {"template": "core/history.html"}, name="history"),
+    url(r'^varsity/', 'core.views.just_render', {"template": "core/varsity.html"}, name="varsity"),
+    url(r'^cuhh/', 'core.views.just_render', {"template": "core/cuhh.html"}, name="cuhh"),
     url(r'^admin/', include(admin.site.urls)),
 )
--- greyhounds/core/views.py
+++ greyhounds/core/views.py
@@ -7,3 +7,11 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'base.html', context)
+
+
+
+def just_render(request, template):
+    """
+        View that takes a template name and just renders it
+    """
+    return render(request, template, {})
