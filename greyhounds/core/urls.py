97d99888d50a7786856caf4e223962bccceadb84
Matt <matt.leach25@gmail.com>
2014-12-18 00:27:52 +0000

    removing chugs bunny

--- greyhounds/core/urls.py
+++ greyhounds/core/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
     url(r'^history/', 'core.views.just_render', {"template": "core/history.html"}, name="history"),
     url(r'^varsity/', 'core.views.just_render', {"template": "core/varsity.html"}, name="varsity"),
     url(r'^cuhh/', 'core.views.just_render', {"template": "core/cuhh.html"}, name="cuhh"),
-    url(r'^chugsbunny/', 'core.views.just_render', {"template": "core/Chugsbunny.html"}, name='Chugsbunny'),
     url(r'^admin/', include(admin.site.urls)),
 )
