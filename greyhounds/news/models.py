11208330990a72b41482c21e717757b12ca7f545
Matt Leach <matt.leach25@gmail.com>
2014-12-20 23:08:39 +0000

    Creating initial model News

---
+++ greyhounds/news/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.DateField(null=True, blank=True)
