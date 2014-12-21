c6151ec3f0a3b78b51865b50ed4f7c16f9bdc487
Matt Leach <matt.leach25@gmail.com>
2014-12-21 15:45:01 +0000

    Adding News admin setting

--- greyhounds/news/models.py
+++ greyhounds/news/models.py
@@ -7,3 +7,6 @@ class News(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     date = models.DateField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.title
