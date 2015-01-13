1b36b4c9a0e77e6bf131931a2ba07b62e8461675
Edd Moffett <edwardmoffett@gmail.com>
2015-01-13 21:31:13 +0000

    afaik, added bcrypt

--- greyhounds/settings.py
+++ greyhounds/settings.py
@@ -90,3 +90,13 @@ STATICFILES_DIRS = (
 STATIC_URL = '/static/'
 
 STATIC_ROOT = '/home/django_apps/static/greyhounds/'
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
